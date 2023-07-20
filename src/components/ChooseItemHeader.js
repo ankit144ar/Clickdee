fade4928aa4d69c4cb7bc609b69489ccc78e662b
Ankit Roy <ankit144ar@gmail.com>
2023-07-20 17:16:48 +0530

    Update ChooseItemHeader.js

--- src/components/ChooseItemHeader.js
+++ src/components/ChooseItemHeader.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ChooseItemHeader() {
   return (
-    <div class="title-container chooseus-title">
+    <div className="title-container chooseus-title">
         <h2>Why the industry chooses Clickdee?</h2>
         <p>We understand performance marketing from every angle and every stage of the funnel. Our clients trust that we know what metrics move their business towards growth. Our publisher and affiliate partners know that we make maximum revenue and ROAS a main focus when growing our partnerships.</p>
     </div>
